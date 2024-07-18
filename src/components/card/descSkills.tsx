f615500ed1ec76ee9d7b17aef36dad46b78396db
Basicbay <keepatbayy@gmail.com>
2024-07-18 17:17:26 +0700

    fix dist img

--- src/components/card/descSkills.tsx
+++ src/components/card/descSkills.tsx
@@ -1,16 +1,29 @@
+import CSS from '../../../public/icon/CSS.svg'
+import HTML from '../../../public/icon/HTML.svg'
+import JavaScript from '../../../public/icon/JavaScript.svg'
+import TypeScript from '../../../public/icon/TypeScript.svg'
+import React from '../../../public/icon/React.svg'
+import NextJS from '../../../public/icon/NextJS.svg'
+import Redux from '../../../public/icon/Redux.svg'
+import NodeJS from '../../../public/icon/NodeJS.svg'
+import TailwindCSS from '../../../public/icon/TailwindCSS.svg'
+import Bootstrap from '../../../public/icon/Bootstrap.svg'
+import Git from '../../../public/icon/Git.svg'
+import Figma from '../../../public/icon/Figma.svg'
+
 const imageNames = [
-  "CSS.svg",
-  "HTML.svg",
-  "JavaScript.svg",
-  "TypeScript.svg",
-  "React.svg",
-  "NextJS.svg",
-  "Redux.svg",
-  "NodeJS.svg",
-  "TailwindCSS.svg",
-  "Bootstrap.svg",
-  "Git.svg",
-  "Figma.svg",
+  CSS,
+  HTML,
+  JavaScript,
+  TypeScript,
+  React,
+  NextJS,
+  Redux,
+  NodeJS,
+  TailwindCSS,
+  Bootstrap,
+  Git,
+  Figma,
 ];
 
 // const imageTitles = [
@@ -35,7 +48,7 @@ export default function DescSkills() {
         <div key={index} className="flex flex-col items-center">
           <img
             key={index}
-            src={`/public/icon/${imageName}`}
+            src={imageName}
             alt="skill"
             className="w-10 h-10"
           />
