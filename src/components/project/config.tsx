eae06e5f9ca18ed66dc63f95c6d423e1fccdb108
Basicbay <keepatbayy@gmail.com>
2024-07-27 16:00:49 +0700

    update profile

--- src/components/project/config.tsx
+++ src/components/project/config.tsx
@@ -1,4 +1,10 @@
 import project from "../../assets/pdf/project.pdf";
+import fwx from "../../assets/images/project/fwx.png";
+import faucet from "../../assets/images/project/faucet.png";
+import user from "../../assets/images/project/user.png";
+import mern from "../../assets/images/project/mern.png";
+import app from "../../assets/images/project/app.png";
+import drone from "../../assets/images/project/drone.png";
 
 interface ProjectItem {
   image: string;
@@ -10,42 +16,42 @@ interface ProjectItem {
 
 export const ProjectItem: ProjectItem[] = [
   {
-    image: "src/assets/images/project/fwx.png",
+    image: fwx,
     tag: "Website",
     key: "FWX Finance",
     desc: "Integrate the new landing page with Next.js Integrate the new landing page with Next.js",
     link: "https://fwx.finance",
   },
   {
-    image: "src/assets/images/project/faucet.png",
+    image: faucet,
     tag: "Web App",
     key: "Faucet Website",
     desc: "Integrate the new landing page with Next.js Integrate the new landing page with Next.js",
     link: "https://fwx.finance",
   },
   {
-    image: "src/assets/images/project/user.png",
+    image: user,
     tag: "Web App",
     key: "User Management",
     desc: "Integrate the new landing page with Next.js Integrate the new landing page with Next.js",
     link: "https://user-management-az98.onrender.com",
   },
   {
-    image: "src/assets/images/project/mern.png",
+    image: mern,
     tag: "Web App",
     key: "Employee Database",
     desc: "Integrate the new landing page with Next.js Integrate the new landing page with Next.js",
     link: "https://mern-stack-crud-m2qd.onrender.com",
   },
   {
-    image: "src/assets/images/project/app.png",
+    image: app,
     tag: "Application",
     key: "Game Application",
     desc: "Integrate the new landing page with Next.js Integrate the new landing page with Next.js",
     link: "https://apps.apple.com/th/developer/nattagrit-ridtikhab/id1288981567?see-all=i-phonei-pad-apps",
   },
   {
-    image: "src/assets/images/project/drone.png",
+    image: drone,
     tag: "Graduation project",
     key: "Computer Vision",
     desc: "Surveys the changing landscape by UAV Surveys the changing landscape by UAV",
